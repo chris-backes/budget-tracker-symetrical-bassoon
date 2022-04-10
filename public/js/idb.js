b434055ea7084a3e96fc2327081fcf17ca33426a
Christopher Backes <Christopher.Backes@gmail.com>
2022-04-10 01:16:13 -0400

    fixed all errors to make it PWA compatible

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,61 +1,62 @@
 let db;
-const request = indexedDB.open("budget_tracker", 1);
 
-request.onupgradeneeded = function (event) {
-	const db = event.target.result;
-	db.createObjectStore("new_transaction", { autoIncrement: true });
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-request.onsuccess = function (event) {
-	db = event.target.result;
-	if (navigator.onLine) {
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
         uploadTransaction();
-	}
+    }
 };
 
-request.onerror = function (event) {
-	console.log(event.target.errorCode);
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
 };
+
 
 function saveRecord(record) {
-	const transaction = db.transaction(["new_transaction"], "readwrite");
-	const transactionObjectStore = transaction.objectStore("new_transaction");
-	transactionObjectStore.add(record);
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const store = transaction.objectStore('new_transaction');
+
+    store.add(record);
 }
 
 function uploadTransaction() {
-	const transaction = db.transaction(["new_transaction"], "readwrite");
-	const transactionObjectStore = transaction.objectStore("new_transaction");
-	const getAll = transactionObjectStore.getAll();
-	getAll.onsuccess = function () {
-		if (getAll.results.length > 0) {
-			fetch("/api/transaction/bulk", {
-				method: "POST",
-				body: JSON.stringify(getAll.result),
-				headers: {
-					Accept: "application/json, text/plain, */*",
-					"Content-Type": "application/json",
-				},
-			})
-				.then((response) => response.json())
-				.then((serverResponse) => {
-					if (serverResponse.message) {
-						throw new Error(serverResponse);
-					}
-					const transaction = db.transaction(
-						["new_transaction"],
-						"readwrite"
-					);
-					const transactionObjectStore =
-						transaction.objectStore("new_transaction");
-					transactionObjectStore.clear();
-					alert("All saved budget items have been submitted!");
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	};
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const store = transaction.objectStore('new_transaction');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function (){
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json' 
+                }
+            })
+            .then(response => {
+                response.json();
+            })
+            .then(() => {
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const store = transaction.objectStore('new_transaction');
+                store.clear();
+
+                alert('all saved budget items have been submitted')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    };
 }
 
 window.addEventListener('online', uploadTransaction);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,18 @@
 const FILES_TO_CACHE = [
+	"/",
 	"./index.html",
-	"./assets/css/style.css",
+	"./css/styles.css",
 	"./js/index.js",
 	"./js/idb.js",
-	"/",
 	"./manifest.json",
-	'./icons/icon-72x72.png',
-	'./icons/icon-96x96.png',
-	'./icons/icon-128x128.png',
-	'./icons/icon-144x144.png',
-	'./icons/icon-152x152.png',
-	'./icons/icon-192x192.png',
-	'./icons/icon-384x384.png',
-	'./icons/icon-512x512.png',
+	"./icons/icon-72x72.png",
+	"./icons/icon-96x96.png",
+	"./icons/icon-128x128.png",
+	"./icons/icon-144x144.png",
+	"./icons/icon-152x152.png",
+	"./icons/icon-192x192.png",
+	"./icons/icon-384x384.png",
+	"./icons/icon-512x512.png",
 ];
 const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -65,16 +65,8 @@ self.addEventListener("fetch", function (evt) {
 		return;
 	}
 	evt.respondWith(
-		fetch(evt.request).catch(function () {
-			return caches.match(evt.request).then(function (response) {
-				if (response) {
-					return response;
-				} else if (
-					evt.request.headers.get("accept").includes("text/html")
-				) {
-					return caches.match("/");
-				}
-			});
+		caches.match(evt.request).then((response) => {
+			return response || fetch(evt.request);
 		})
 	);
 });
